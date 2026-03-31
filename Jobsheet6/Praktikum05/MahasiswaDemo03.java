205f2e7d6f0ad6d452a2a20c5882f335a84316f4
Pidddd <424f1d@gmail.com>
2026-03-31 07:53:32 +0700

    5.3.5 Mengurutkan Data Mahasiswa Berdasarkan IPK (Selection Sort)

--- Jobsheet6/Praktikum05/MahasiswaDemo03.java
+++ Jobsheet6/Praktikum05/MahasiswaDemo03.java
@@ -4,11 +4,11 @@ public class MahasiswaDemo03 {
     public static void main(String[] args) {
         MahasiswaBerprestasi03 list = new MahasiswaBerprestasi03();
         
-        Mahasiswa03 m1 = new Mahasiswa03("123", "Zidan", "2A", 3.2);
-        Mahasiswa03 m2 = new Mahasiswa03("124", "Ayu", "2A", 3.5);
-        Mahasiswa03 m3 = new Mahasiswa03("125", "Sofi", "2A", 3.1);
-        Mahasiswa03 m4 = new Mahasiswa03("126", "Sita", "2A", 3.9);
-        Mahasiswa03 m5 = new Mahasiswa03("127", "Miki", "2A", 3.7);
+        Mahasiswa03 m1 = new Mahasiswa03("123", "Zicco", "2A", 3.2);
+        Mahasiswa03 m2 = new Mahasiswa03("124", "Akbar", "2A", 3.5);
+        Mahasiswa03 m3 = new Mahasiswa03("125", "Daffa", "2A", 3.1);
+        Mahasiswa03 m4 = new Mahasiswa03("126", "Zulfikar", "2A", 3.9);
+        Mahasiswa03 m5 = new Mahasiswa03("127", "Repan", "2A", 3.7);
 
         list.tambah(m1);
         list.tambah(m2);
@@ -21,6 +21,10 @@ public class MahasiswaDemo03 {
 
         System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC): ");
+        list.selectionSort();
         list.tampil();
     }
 }
