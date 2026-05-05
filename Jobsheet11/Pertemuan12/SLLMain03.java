38f73f55eff15683f4881f841ce06ebbfcb349a8
Pidddd <424f1d@gmail.com>
2026-05-05 08:20:33 +0700

    Praktikum 2.1

---
+++ Jobsheet11/Pertemuan12/SLLMain03.java
@@ -0,0 +1,34 @@
+package Jobsheet11.Pertemuan12;
+
+import java.util.Scanner;
+
+public class SLLMain03 {
+    public static void main(String[] args) {
+        SingleLinkedList03 sll = new SingleLinkedList03();
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.println("=== Tambah Data Mahasiswa ===");
+        System.out.print("Masukkan NIM   : ");
+        String nim = sc.nextLine();
+        
+        System.out.print("Masukkan Nama  : ");
+        String nama = sc.nextLine();
+        
+        System.out.print("Masukkan Kelas : ");
+        String kelas = sc.nextLine();
+        
+        System.out.print("Masukkan IPK   : ");
+        double ipk = sc.nextDouble();
+
+        // Buat object mahasiswa baru dari data yang diinputkan
+        Mahasiswa03 mhsBaru = new Mahasiswa03(nim, nama, kelas, ipk);
+
+        // Tambahkan ke dalam Linked List (misal ditaruh di paling belakang)
+        sll.addLast(mhsBaru);
+
+        System.out.println("\nData setelah ditambahkan:");
+        sll.print(); 
+
+        sc.close();
+    }
+}
