bbae47dec24806d8e77d8e0b48a503ad17a5641f
Pidddd <424f1d@gmail.com>
2026-05-26 08:04:59 +0700

    Praktikum 1

---
+++ Jobsheet14/Node03.java
@@ -0,0 +1,15 @@
+package Jobsheet14;
+
+public class Node03 {
+    Mahasiswa03 mahasiswa;
+    Node03 left, right;
+
+    public Node03() {
+    }
+
+    public Node03 (Mahasiswa03 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        this.left = null;
+        this.right = null;
+    }
+}
