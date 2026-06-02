f67919a55af078164e75dea0206b197617985c56
Pidddd <424f1d@gmail.com>
2026-06-02 08:59:39 +0700

    13.2 Persiapan

---
+++ Jobsheet15/Customer03.java
@@ -0,0 +1,17 @@
+package Jobsheet15;
+
+public class Customer03 {
+    public int id;
+    public String name;
+
+    public Customer03() {}
+
+    public Customer03(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "ID: " + this.id + ", Nama: " + this.name;
+    }
+}
